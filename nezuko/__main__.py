8eb11845ae8c932d678f31dae83a292b85a1fc87
allu <62532710+allu725@users.noreply.github.com>
2022-07-25 14:00:39 +0530

    Update __main__.py

--- nezuko/__main__.py
+++ nezuko/__main__.py
@@ -107,8 +107,8 @@ home_keyboard_pm = InlineKeyboardMarkup(
                 text="Commands ❓", callback_data="bot_commands"
             ),
             InlineKeyboardButton(
-                text="Repo ❤️",
-                url="https://github.com/rozari0/NezukoBot",
+                text="creator ❤️",
+                url="https://t.me/optimus_prime_althaf",
             ),
         ],
         [
@@ -141,8 +141,8 @@ keyboard = InlineKeyboardMarkup(
                 url=f"t.me/{BOT_USERNAME}?start=help",
             ),
             InlineKeyboardButton(
-                text="Repo 🛠",
-                url="https://github.com/rozari0/NezukoBot",
+                text="creator",
+                url="https://t.me/optimus_prime_althaf",
             ),
         ],
         [
